451c6751ac9e02d3883f33e144f61aa9e672f8c7
Sergio Acosta <sergio@sergioacosta.net>
2016-03-11 12:36:22 -0600

    Mejoras

--- ISBN/ViewController.swift
+++ ISBN/ViewController.swift
@@ -49,6 +49,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    func parse(data: NSDictionary) -> (title: String, authors: [String]?, cover: NSURL?){
+        var bookTitle = ""
+        var bookAuthors: [String] = []
+        var bookCover: NSURL? = nil
+        
+        let elements = data["ISBN:" + self.ISBN.text!] as! NSDictionary
+        if let t = elements["title"] {
+            bookTitle = t as! String
+        }
+        if let auth = elements["authors"] {
+            for a in auth as! NSArray {
+                if let n = a["name"] {
+                    bookAuthors.append(n as! String)
+                }
+            }
+        }
+        if let c = elements["cover"] {
+            if let m = c["medium"] {
+                bookCover = NSURL(string: m as! String)
+            }
+        }
+        return (bookTitle, bookAuthors, bookCover)
+    }
+    
     func search() {
         ISBN.resignFirstResponder()
         let url = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + ISBN.text!)
@@ -57,9 +81,42 @@ class ViewController: UIViewController, UITextFieldDelegate {
             if let httpResponse = response as? NSHTTPURLResponse {
                 if httpResponse.statusCode == 200 {
                     do {
-                        let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableLeaves)
-                        let jsonData = json as! NSDictionary
-                        if jsonData.count <= 0 {
+                        let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableLeaves) as! NSDictionary
+                        if json.count > 0 {
+                            let bookData = self.parse(json)
+                            dispatch_sync(dispatch_get_main_queue()) {
+                                self.Results.text = "Libro:\t" + (bookData.title) + "\n"
+                                
+                                if let authors = bookData.authors {
+                                    if authors.count == 1 {
+                                        self.Results.text = self.Results.text + "Autor:\t" + authors[0] + "\n"
+                                    } else if authors.count > 1 {
+                                        self.Results.text = self.Results.text + "Autores:\t"
+                                        for author in authors {
+                                            self.Results.text = self.Results.text + author
+                                            if author != authors.last {
+                                                self.Results.text = self.Results.text + ", "
+                                            }
+                                        }
+                                        self.Results.text = self.Results.text + "\n"
+                                    }
+                                }
+                                if let cover = bookData.cover {
+                                    if let data = NSData(contentsOfURL: cover) {
+                                        self.noCoverLabel.hidden = true
+                                        self.imageView.contentMode = .ScaleAspectFit
+                                        self.imageView.image = UIImage(data: data)
+                                    }
+                                    self.indicator.stopAnimating()
+                                    self.ISBN.userInteractionEnabled = true
+                                } else {
+                                    self.imageView.image = nil
+                                    self.noCoverLabel.hidden = false
+                                    self.indicator.stopAnimating()
+                                    self.ISBN.userInteractionEnabled = true
+                                }
+                            }
+                        } else {
                             dispatch_sync(dispatch_get_main_queue()) {
                                 self.Results.text = "No hay resultados."
                                 self.imageView.image = nil
@@ -67,47 +124,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
                                 self.indicator.stopAnimating()
                                 self.ISBN.userInteractionEnabled = true
                             }
-                        } else {
-                            let elements = jsonData["ISBN:" + self.ISBN.text!] as! NSDictionary
-                            let book = elements["title"] as! NSString
-                            dispatch_sync(dispatch_get_main_queue()) {
-                                self.Results.text = "Libro:\t" + (book as String) + "\n"
-                            }
-                            if let authors = elements["authors"] {
-                                dispatch_sync(dispatch_get_main_queue()) {
-                                    if authors.count == 1 {
-                                        let author = authors[0] as! NSDictionary
-                                        self.Results.text = self.Results.text + "Autor:\t" + (author["name"] as! String) + "\n"
-                                    } else if authors.count > 1 {
-                                        self.Results.text = self.Results.text + "Autores:\t"
-                                        for i in 0..<authors.count {
-                                            self.Results.text = self.Results.text + (authors[i]["name"] as! String)
-                                            if i < authors.count - 1 {
-                                                self.Results.text = self.Results.text + ", "
-                                            }
-                                        }
-                                        self.Results.text = self.Results.text + "\n"
-                                    }
-                                }
-                            }
-                            if elements["cover"] != nil {
-                                if let url = NSURL(string: elements["cover"]!["medium"] as! String) {
-                                    if let data = NSData(contentsOfURL: url) {
-                                        self.noCoverLabel.hidden = true
-                                        self.imageView.contentMode = .ScaleAspectFit
-                                        self.imageView.image = UIImage(data: data)
-                                    }
-                                } else {
-                                    self.imageView.image = nil
-                                    self.noCoverLabel.hidden = false
-                                }
-                            } else {
-                                self.noCoverLabel.hidden = false
-                            }
-                        }
-                        dispatch_sync(dispatch_get_main_queue()) {
-                            self.indicator.stopAnimating()
-                            self.ISBN.userInteractionEnabled = true
                         }
                     } catch _ {}
                 } else {
